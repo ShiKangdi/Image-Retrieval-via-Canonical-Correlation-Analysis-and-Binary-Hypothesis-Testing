d67bc9630799277db2ab79d449892360ca39d034
Kangdi Shi <37673163+ShiKangdi@users.noreply.github.com>
2022-02-11 08:43:54 -0500

    Update Evaluation_G-CCA.py

--- Evaluation_G-CCA.py
+++ Evaluation_G-CCA.py
@@ -112,8 +112,8 @@ if __name__ == '__main__':
                     B = (CV_X**2) + (CV_Y**2) - 2*CV_X*CV_Y*CN   
                     C = np.sqrt((1-(CM**2))
                     D = np.sqrt((1-(CN**2))
-                    E = m.log(D/C)
-                    F = -(A/C) + (B/D) + E
+                   # E = m.log(D/C)
+                    F = -(A/C) + (B/D) #+ E
                           
                     P = P + F 
                     
