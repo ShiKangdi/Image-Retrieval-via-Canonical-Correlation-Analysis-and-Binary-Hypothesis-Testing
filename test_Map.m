d9c8442ffc578ff6de1745c195080eb97ca9645b
Kangdi Shi <37673163+ShiKangdi@users.noreply.github.com>
2022-02-13 12:09:46 -0500

    Update test_Map.m

--- test_Map.m
+++ test_Map.m
@@ -7,7 +7,7 @@ Folder = '' % Place Your Folder address in here. The folder stores the results i
 dim = [512,450,400,300,200,100,50,25];
 for n = 1:length(dim)
     for m = 0:54
-        fid = fopen(strcat(Folder + '/query',string(m),'.txt'));
+        fid = fopen(strcat(Folder + 'Max' + str(dim(n)) +'/query',string(m),'.txt'));
         data = textscan(fid,'%s'); 
         fclose(fid);
         for j = 1:length(imlist)
